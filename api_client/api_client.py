ee7d90dc2b674a3f1be4c28ca55ba4ab146ee9e0
Виталий Личинин <lichinin.v@yandex.ru>
2025-06-02 20:35:27 +0300

    Переделал ApiClient на LazyInitialization

--- api_client/api_client.py
+++ api_client/api_client.py
@@ -5,6 +5,24 @@ from api_client.resources.user import UserClient
 
 class ApiClient():
     def __init__(self):
-        self.posts = PostClient()
-        self.comments = CommentClient()
-        self.users = UserClient()
+        self._posts = None
+        self._comments = None
+        self._users = None
+
+    @property
+    def posts(self):
+        if self._posts is None:
+            self._posts = PostClient()
+        return self._posts
+
+    @property
+    def comments(self):
+        if self._comments is None:
+            self._comments = CommentClient()
+        return self._comments
+
+    @property
+    def users(self):
+        if self._users is None:
+            self._users = UserClient()
+        return self._users
